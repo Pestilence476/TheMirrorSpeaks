5d1a121fdeb3fa83f9feecad05e2173f1244d470
Pestilence476 <pestilence476@gmail.com>
2026-03-04 22:56:23 -0600

    Major changesssss

--- Assets/Scripts/TextCreator.cs
+++ Assets/Scripts/TextCreator.cs
@@ -4,27 +4,54 @@ using UnityEngine;
 public class TextCreator : MonoBehaviour
 {
     public static TMPro.TMP_Text viewText;
+    public static TMPro.TMP_Text viewText2;
     public static bool runTextPrint;
+    public static bool runTextPrint2;
     public static int charCount;
     [SerializeField] string transferText;
+    [SerializeField] string transferText2;
     [SerializeField] int internalCount;
     public float speed = 0;
 
     // Update is called once per frame
     void Update()
     {
-        internalCount = charCount;
-        charCount = GetComponent<TMPro.TMP_Text>().text.Length;
-        if(runTextPrint == true)
+        if (gameObject.name == "SpeakText")
         {
-            runTextPrint = false;
-            viewText = GetComponent<TMPro.TMP_Text>();
-            transferText = viewText.text;
-            viewText.text = "";
-            StartCoroutine(RollText());
+            internalCount = charCount;
+            charCount = GetComponent<TMPro.TMP_Text>().text.Length;
+            if (runTextPrint == true)
+            {
+                runTextPrint = false;
+                viewText = GetComponent<TMPro.TMP_Text>();
+                transferText = viewText.text;
+                viewText.text = "";
+                StartCoroutine(RollText());
+            }
+        }
+        if (gameObject.name == "CharName")
+        {
+            if (runTextPrint2 == true)
+            {
+                runTextPrint2 = false;
+                viewText2 = GetComponent<TMPro.TMP_Text>();
+                transferText2 = viewText2.text;
+                viewText2.text = "";
+                StartCoroutine(PlaceText());
+            }
         }
     }
 
+
+    IEnumerator PlaceText()
+    {
+
+        foreach (char c in transferText2)
+        {
+            viewText2.text += c;
+            yield return new WaitForSeconds(0);
+        }
+    }
 
     IEnumerator RollText()
     {
