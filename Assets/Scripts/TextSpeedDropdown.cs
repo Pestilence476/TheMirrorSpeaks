176426a32511828b082135063b49ecf1ee9bd401
Pestilence476 <pestilence476@gmail.com>
2026-03-07 12:32:39 -0600

    Text speed can now be changed

--- Assets/Scripts/TextSpeedDropdown.cs
+++ Assets/Scripts/TextSpeedDropdown.cs
@@ -9,9 +9,11 @@ using UnityEngine.UI;
 public class TextSpeedDropdown : MonoBehaviour
 {
     public TMP_Text textbox;
+    public static TextSpeedDropdown Instance;
 
     void Start()
     {
+        
         var dropdown = transform.GetComponent<TMP_Dropdown>();
 
         dropdown.options.Clear();
@@ -39,5 +41,22 @@ public class TextSpeedDropdown : MonoBehaviour
     {
         int index = dropdown.value;
         textbox.text = dropdown.options[index].text;
+        UpdateTextSpeed();
+    }
+
+    public void UpdateTextSpeed()
+    {
+        if (textbox.text == "Normal")
+        {
+            GameData.SetTextSpeed(0.03f);
+        }
+        if (textbox.text == "Medium")
+        {
+            GameData.SetTextSpeed(-50f);
+        }
+        if (textbox.text == "Fast")
+        {
+            GameData.SetTextSpeed(-500f);
+        }
     }
 }
